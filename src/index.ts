6877f59bb7648646ecc4b3b24c1c68328b9eb419
Rido Atmanto <ridoatmanto@gmail.com>
2024-06-24 01:02:35 +0700

    feat: list and search book

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { prisma } from "./libs/prisma";
 
 import {
   type Book,
@@ -9,7 +10,7 @@ import {
 } from "./data/books";
 import { SortByDate } from "./utils/sort-by-date";
 
-let books = dataBooks;
+// let books = dataBooks;
 
 const app = new Hono();
 
@@ -20,127 +21,155 @@ app.get("/", (c) => {
   });
 });
 
-app.get("/books", (c) => {
-  const sortedBooks = SortByDate(books);
-  const completeBooks: CompleteBook[] = [];
-  sortedBooks.forEach((book: Book) => {
-    let currentBook = book;
-    Object.assign(currentBook, { prop: "authors" });
-    currentBook.authors = [getAuthorDetail(book.author_id)];
-    completeBooks.push(currentBook);
-  });
+app.get("/books", async (c) => {
+  try {
+    const search = c.req.query("q");
 
-  return c.json(completeBooks);
-});
+    const allBooks = await prisma.book.findMany({
+      where: {},
+      orderBy: [{ created_at: "desc" }],
+    });
 
-app.get("/books/:id", (c) => {
-  const book = books.filter((book) => book.id == c.req.param("id"));
-
-  Object.assign(book, { prop: "authors" });
-  book.authors = [getAuthorDetail(book.author_id)];
-
-  return c.json(book);
-});
-
-app.post("/books", async (c) => {
-  const body = await c.req.json();
-  const newBook: Book = {
-    id: randomId(),
-    title: body.title,
-    description: body.description,
-    author_id: body.author_id,
-    published: body.published,
-    cover: "",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-  const newBooksList = [...books, newBook];
-  books = newBooksList;
-
-  Object.assign(newBook, { prop: "authors" });
-  newBook.authors = [getAuthorDetail(newBook.author_id)];
-
-  return c.json(newBook);
-});
-
-app.delete("/books/:id", (c) => {
-  const id = c.req.param("id");
-
-  if (!id) {
-    return c.json({ message: "Books ID needed before delete!" });
-  }
-
-  const deletedBook = books.find((book) => book.id === id);
-
-  if (!deletedBook) {
-    return c.json({ message: `Books with ID: '${id}' not found.` });
-  }
-
-  books = books.filter((book) => book.id !== id);
-
-  return c.json({
-    message: `Book with ID: '${id}' has been deleted!`,
-    deletedBook: deletedBook,
-  });
-});
-
-app.put("/books/:id", async (c) => {
-  const id = c.req.param("id");
-  const body = await c.req.json();
-
-  if (!id) {
-    return c.json({ message: "Books ID needed before delete!" });
-  }
-
-  const updatedBook = books.find((book: Book) => book.id === id);
-
-  if (!updatedBook) {
-    return c.json({ message: `Books with ID: '${id}' not found.` });
-  }
-
-  const submittedBook: Book = {
-    id: updatedBook.id,
-    title: body.title,
-    description: body.description,
-    author_id: body.author_id,
-    published: body.published,
-    cover: "",
-    createdAt: updatedBook.createdAt,
-    updatedAt: new Date(),
-  };
-
-  const latestBook = books.map((book: Book) => {
-    if (book.id === id) {
-      return submittedBook;
-    } else {
-      return book;
+    if (search == null || search == undefined) {
+      return c.json(allBooks);
     }
-  });
 
-  books = latestBook;
+    const searchBook = await prisma.$queryRawUnsafe(`SELECT * from "Book" WHERE 
+      LOWER(title) LIKE '%${search.toLowerCase()}%'`);
 
-  return c.json({
-    message: `Book with ID: ${id} has been updated!`,
-    deletedBook: submittedBook,
-  });
-});
-
-app.delete("/books", (c) => {
-  books = [];
-  return c.json(books);
-});
-
-function randomId() {
-  return Math.random().toString(36).slice(2, 7);
-}
-
-function getAuthorDetail(authorId: string) {
-  const result = dataAuthors.find((author: Author) => author.id === authorId);
-  if (!result) {
-    return [];
+    return c.json(searchBook);
+  } catch (err: any) {
+    console.log(err.message);
   }
+});
 
-  return result;
-}
+// app.get("/books/:q?", async (c) => {
+//   console.log('c.req.param("q")');
+//   console.log(c.req.param("q"));
+//   try {
+//     const allBooks = await prisma.book.findMany({
+//       where: {
+//         title: {
+//           search: c.req.param("q"),
+//         },
+//       },
+//       orderBy: [{ created_at: "desc" }],
+//     });
+
+//     return c.json(allBooks);
+//   } catch (err: any) {
+//     console.log(err.message);
+//   }
+// });
+
+// app.get("/books/:id", (c) => {
+//   const book = books.filter((book) => book.id == c.req.param("id"));
+
+//   Object.assign(book, { prop: "authors" });
+//   book.authors = [getAuthorDetail(book.author_id)];
+
+//   return c.json(book);
+// });
+
+// app.post("/books", async (c) => {
+//   const body = await c.req.json();
+//   const newBook: Book = {
+//     id: randomId(),
+//     title: body.title,
+//     description: body.description,
+//     author_id: body.author_id,
+//     published: body.published,
+//     cover: "",
+//     createdAt: new Date(),
+//     updatedAt: new Date(),
+//   };
+//   const newBooksList = [...books, newBook];
+//   books = newBooksList;
+
+//   Object.assign(newBook, { prop: "authors" });
+//   newBook.authors = [getAuthorDetail(newBook.author_id)];
+
+//   return c.json(newBook);
+// });
+
+// app.delete("/books/:id", (c) => {
+//   const id = c.req.param("id");
+
+//   if (!id) {
+//     return c.json({ message: "Books ID needed before delete!" });
+//   }
+
+//   const deletedBook = books.find((book) => book.id === id);
+
+//   if (!deletedBook) {
+//     return c.json({ message: `Books with ID: '${id}' not found.` });
+//   }
+
+//   books = books.filter((book) => book.id !== id);
+
+//   return c.json({
+//     message: `Book with ID: '${id}' has been deleted!`,
+//     deletedBook: deletedBook,
+//   });
+// });
+
+// app.put("/books/:id", async (c) => {
+//   const id = c.req.param("id");
+//   const body = await c.req.json();
+
+//   if (!id) {
+//     return c.json({ message: "Books ID needed before delete!" });
+//   }
+
+//   const updatedBook = books.find((book: Book) => book.id === id);
+
+//   if (!updatedBook) {
+//     return c.json({ message: `Books with ID: '${id}' not found.` });
+//   }
+
+//   const submittedBook: Book = {
+//     id: updatedBook.id,
+//     title: body.title,
+//     description: body.description,
+//     author_id: body.author_id,
+//     published: body.published,
+//     cover: "",
+//     createdAt: updatedBook.createdAt,
+//     updatedAt: new Date(),
+//   };
+
+//   const latestBook = books.map((book: Book) => {
+//     if (book.id === id) {
+//       return submittedBook;
+//     } else {
+//       return book;
+//     }
+//   });
+
+//   books = latestBook;
+
+//   return c.json({
+//     message: `Book with ID: ${id} has been updated!`,
+//     deletedBook: submittedBook,
+//   });
+// });
+
+// app.delete("/books", (c) => {
+//   books = [];
+//   return c.json(books);
+// });
+
+// function randomId() {
+//   return Math.random().toString(36).slice(2, 7);
+// }
+
+// function getAuthorDetail(authorId: string) {
+//   const result = dataAuthors.find((author: Author) => author.id === authorId);
+//   if (!result) {
+//     return [];
+//   }
+
+//   return result;
+// }
 
 export default app;
