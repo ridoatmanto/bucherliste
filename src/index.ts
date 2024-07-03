c658bcc9c5d03aa8fb1b3f5d3d7ed7b517c97b41
Rido Atmanto <ridoatmanto@gmail.com>
2024-07-03 21:10:51 +0700

    feat: refactor name convention

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { books } from "./books";
 import { authors } from "./authors";
-import { book_authors } from "./book_authors";
+import { bookAuthors } from "./book-authors";
 
 const app = new Hono();
 
@@ -10,12 +10,12 @@ app.get("/", (c) => {
     message: "Bucherliste",
     books: "/books",
     authors: "/authors",
-    book_authors: "/book_authors",
+    "book-authors": "/book-authors",
   });
 });
 
 app.route("/books", books);
 app.route("/authors", authors);
-app.route("/book_authors", book_authors);
+app.route("/book-authors", bookAuthors);
 
 export default app;
